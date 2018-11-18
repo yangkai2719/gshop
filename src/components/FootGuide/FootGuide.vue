7919700b336167cb9367cb43dfdfefe797747cd2
yangkai <408786028@qq.com>
2018-11-18 15:35:40 +0800

    '第一次提交'

---
+++ src/components/FootGuide/FootGuide.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer class="footer">
+    <span class="guide_item on" :class="{on:$route.path==='/msite'}" @click="goto('/msite')"><span><i
+      class="iconfont icon-waimai"></i></span>
+      <span class="itemText">外卖</span>
+    </span>
+    <span class="guide_item" :class="{on:$route.path==='/search'}" @click="goto('/search')"><span><i
+      class="iconfont icon-search"></i></span>
+      <span class="itemText">搜索</span>
+    </span>
+    <span class="guide_item" :class="{on:$route.path==='/order'}" @click="goto('/order')"><span><i
+      class="iconfont icon-dingdan"></i></span>
+      <span class="itemText">订单</span>
+    </span>
+    <span class="guide_item" :class="{on:$route.path==='/profile'}" @click="goto('/profile')"><span><i
+      class="iconfont icon-geren"></i></span>
+      <span class="itemText">我的</span>
+    </span>
+  </footer>
+</template>
+
+<script>
+  export default {
+    methods: {
+      goto(path){
+        this.$router.replace(path)
+      }
+
+
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .footer
+    top-border-1px(#ddd)
+    position fixed
+    bottom 0
+    left 0
+    width 100%
+    height 50px
+    display flex
+    .footerItem
+      flex 1
+      display flex
+      justify-content center
+      flex-direction column
+      align-items center
+      .iconfont
+        font-size 22px
+      .itemText
+        font-size 13px
+        margin 3px 0
+      &.active
+        color $maincolor
+
+
+</style>
+
---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>FootGuide</div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
+
+
